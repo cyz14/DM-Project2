91e28b3eb0c24e3f61e5cb2f3ebdc4f10413960b
tonygao <gaotong_sh@foxmail.com>
2017-06-25 22:38:50 +0800

    Update const.py

--- script/const.py
+++ script/const.py
@@ -1,4 +1,4 @@
 
-corpus_dir = "../../5_project2/samples_50000"
-trash_dir = "../../5_project2/trash_doc"
-DATASET_SIZE = 1000
+corpus_dir = "../samples_50000"
+trash_dir = "../trash_doc"
+DATASET_SIZE = 50000
